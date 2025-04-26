4e4d1b839e6aa78e050393f55d2ea2d72074e02e
Mike Gerard Suico <mgsuico.mentorspire@gmail.com>
2025-04-26 11:13:52 +0800

    Minor changes

--- app/components/card/OfferCard.tsx
+++ app/components/card/OfferCard.tsx
@@ -12,8 +12,7 @@ const OfferCard: React.FC<OfferCardProps> = ({ id, title , image, description, u
     return (
         <div className="bg-white rounded-xl overflow-hidden shadow-md border p-4 w-full my-10">
             <h1 className="text-xl md:text-2xl font-bold mb-5"><span className='bg-gray-600 px-3 py-1 rounded-lg text-white text-base md:text-xl'>{id}</span> {title}</h1>
-            <img src={image} alt={title} className="w-full h-48 object-cover rounded-md" />
-      
+            <img src={image} alt={title} className="w-full h-25 md:h-48 object-cover object-center rounded-lg" />
             <p
                 className="text-base text-gray-700 mt-5"
                 dangerouslySetInnerHTML={{ __html: description }}
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='relative w-full h-[500px] max-h-[500px] bg-[url(/images/article-header-resized.png)] bg-cover bg-center bg-no-repeat'>
         <Header />
         <div className="absolute inset-0 bg-black/50 z-0"></div>
-        <div className="relative z-10 p-6 text-white w-full md:max-w-[780px] m-auto pt-15">
+        <div className="relative z-10 p-6 text-white w-full md:max-w-[780px] m-auto pt-10">
             <h6 className="text-lg uppercase mb-2 text-center font-semibold">Advertisement</h6>
             <h1 className="text-4xl md:text-6xl font-black text-center">Limited-Time Supplements at Their Lowest Prices Today That You Won’t Believe! </h1>
         </div>
