818724e354cc1cc5d36bca3ee30f1c758de0ebf1
Mike Gerard Suico <mgsuico.mentorspire@gmail.com>
2025-05-02 08:43:18 +0800

    Removed unnecessary files - Added SeachParamas Component

---
+++ app/components/search/SearchParamsContent.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { useSearchParams } from '@/node_modules/next/navigation';
+import OfferCard from "../card/OfferCard";
+import OtherCard from "../card/OtherCard";
+import { products, otherOffers } from "@/static/datas";
+import React from "react";
+
+const SearchParamsContent = () => {
+    const searchParams = useSearchParams();
+    const queryString = searchParams.toString();
+
+  return (
+    <>
+      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
+        <p className="text-lg">
+          Looking to find the best natural supplement deals without breaking the bank?
+          We have done the hard work for you by hunting down these absolute best online deals right now!
+          Our team of shopping experts has uncovered these incredible products at their
+          <span className="font-bold underline"> lowest prices ever</span> – we’re talking huge discounts up to 75% off retail prices!
+        </p>
+      </div>
+
+      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
+        {products.map((offer) => {
+          const fullUrl = queryString ? `${offer.url}?${queryString}` : offer.url;
+
+          return (
+            <React.Fragment key={offer.id}>
+              <hr />
+              <OfferCard {...offer} url={fullUrl} />
+            </React.Fragment>
+          );
+        })}
+      </div>
+
+      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
+        <hr />
+        <h6 className='py-5 font-bold'>You May Also Like</h6>
+        <div className='mb-10 flex justify-center items-center md:block'>
+          {otherOffers.map((otherOffer) => {
+            const fullUrl = queryString ? `${otherOffer.url}?${queryString}` : otherOffer.url;
+
+            return (
+              <React.Fragment key={otherOffer.id}>
+                <hr />
+                <OtherCard {...otherOffer} url={fullUrl} />
+              </React.Fragment>
+            );
+          })}
+        </div>
+        <hr />
+      </div>
+    </>
+  )
+}
+
+export default SearchParamsContent
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,23 @@
-'use client';
-
-import { useSearchParams } from "@/node_modules/next/navigation";
 import Intro from "./components/intro/Intro";
-import OtherCard from "./components/card/OtherCard";
-import OfferCard from "./components/card/OfferCard";
 import Footer from "./components/footer/Footer";
-import { Product, products, otherOffers } from "@/static/datas";
-import React from "react";
+import SearchParamsContent from "./components/search/SearchParamsContent";
+import React, { Suspense } from "react";
 
+const Spinner = () => (
+  <div className="flex justify-center items-center h-screen">
+    <div className="border-t-4 border-blue-500 border-solid w-16 h-16 rounded-full animate-spin"></div>
+  </div>
+);
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const queryString = searchParams.toString();
 
   return (
     <div className="w-full h-auto overflow-x-hidden">
       <Intro />
-      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
-        <p className="text-lg">Looking to find the best natural supplement deals without breaking the bank? We have done the hard work for you by hunting down these absolute best online deals right now! Our team of shopping experts has uncovered these incredible products at their <span className="font-bold underline">lowest prices ever</span> – we’re talking huge discounts up to 75% off retail prices! Discover the natural solutions that have been helping thousands of Americans feel their best every day now available at a special promotional pricing for a limited time today!</p>
-      </div>
-      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
-        {products.map((offer) => {
-          const fullUrl = queryString ? `${offer.url}?${queryString}` : offer.url;
-
-          return (
-            <React.Fragment key={offer.id}>
-              <hr />
-              <OfferCard {...offer} url={fullUrl} />
-            </React.Fragment>
-          );
-        })}
-      </div>
-      <div className="w-[90%] md:w-full md:max-w-[780px] m-auto mt-10">
-        <hr />
-        <h6 className='py-5 font-bold'>You May Also Like</h6>
-        <div className='mb-10 flex justify-center items-center md:block'>
-          {otherOffers.map((otherOffer) => {
-            const fullUrl = queryString ? `${otherOffer.url}?${queryString}` : otherOffer.url;
-
-            return (
-              <React.Fragment key={otherOffer.id}>
-                <hr />
-                <OtherCard {...otherOffer} url={fullUrl} />
-              </React.Fragment>
-            );
-          })}
-        </div>
-        <hr />
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <SearchParamsContent />
       <Footer />
+      </Suspense>
     </div>
   );
 }
